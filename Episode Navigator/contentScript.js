98d3eeddfeb696256aecc8aaca6a23113564a43c
hankftw <37029415+hankftw@users.noreply.github.com>
2024-03-24 19:56:38 +0000

    Update contentScript.js

--- Episode Navigator/contentScript.js
+++ Episode Navigator/contentScript.js
@@ -1,32 +1,14 @@
+// contentScript.js
+
 var urlElements = document.querySelectorAll('[href]');
 var nextChapterElement = Array.from(urlElements).find(element => {
   return element.textContent.toLowerCase().includes('next');
 });
 
 if (nextChapterElement) {
-  var nextChapterLink = nextChapterElement.href; 
+  var nextChapterLink = nextChapterElement.href;
+ 
   chrome.runtime.sendMessage({ type: "updateTab", url: nextChapterLink });
+} else {
+  console.error("Next chapter element not found.");
 }
-//background.js
-chrome.commands.onCommand.addListener(function(command) {
-  if (command == "navigate-forward") {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      if (!tabs || tabs.length === 0) {       
-        return;
-      }
-
-      var tabId = tabs[0].id;
-
-      chrome.scripting.executeScript({
-        target: {tabId: tabId},
-        files: ['contentScript.js']
-      });
-    });
-  }
-});
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.type === "updateTab") {
-    chrome.tabs.update(sender.tab.id, {url: message.url});
-  }
-});
